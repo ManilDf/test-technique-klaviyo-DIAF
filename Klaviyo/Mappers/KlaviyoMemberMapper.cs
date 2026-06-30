86c9419cb3135fc375273d91933fac59f09b2694
ManilDf <diaf.manil@gmail.com>
2026-06-30 19:13:38 +0200

    feat: implement KlaviyoMemberMapper (les tests passent)

--- Klaviyo/Mappers/KlaviyoMemberMapper.cs
+++ Klaviyo/Mappers/KlaviyoMemberMapper.cs
@@ -16,6 +16,70 @@ public class KlaviyoMemberMapper : IKlaviyoMemberMapper
         _connectionFactory = connectionFactory;
     }
 
-    public Task<KlaviyoMember> GetMemberAsync(int connectionId, int memberId)
-        => throw new NotImplementedException();
+    public async Task<KlaviyoMember> GetMemberAsync(int connectionId, int memberId)
+    {
+        var connectionString = _connectionResolver.GetConnectionString(connectionId);
+        await using var connection = _connectionFactory.CreateConnection(connectionString);
+        await connection.OpenAsync();
+
+        await using var command = connection.CreateCommand();
+        command.CommandText = @"
+            SELECT
+                m.FirstName, m.LastName, m.PhoneNumber, m.BirthDate,
+                m.City, m.Country, m.LoyaltyPoints, m.EnrolledAt,
+                me.EmailAddress
+            FROM Member m
+            LEFT JOIN MemberEmail me ON me.MemberEmailId = m.MainEmailAddressId
+            WHERE m.MemberId = @MemberId";
+
+        var param = command.CreateParameter();
+        param.ParameterName = "@MemberId";
+        param.Value = memberId;
+        command.Parameters.Add(param);
+
+        await using var reader = await command.ExecuteReaderAsync();
+
+        if (!await reader.ReadAsync())
+            throw new KeyNotFoundException($"Member {memberId} not found.");
+
+        var phoneOrdinal    = reader.GetOrdinal("PhoneNumber");
+        var birthOrdinal    = reader.GetOrdinal("BirthDate");
+        var cityOrdinal     = reader.GetOrdinal("City");
+        var countryOrdinal  = reader.GetOrdinal("Country");
+        var emailOrdinal    = reader.GetOrdinal("EmailAddress");
+        var enrolledOrdinal = reader.GetOrdinal("EnrolledAt");
+
+        var city    = reader.IsDBNull(cityOrdinal)    ? null : reader.GetString(cityOrdinal);
+        var country = reader.IsDBNull(countryOrdinal) ? null : reader.GetString(countryOrdinal);
+
+        var birthDate = reader.IsDBNull(birthOrdinal)
+            ? null
+            : reader.GetDateTime(birthOrdinal).ToString("yyyy-MM-dd");
+
+        var enrolledAt = DateTime.SpecifyKind(reader.GetDateTime(enrolledOrdinal), DateTimeKind.Utc)
+            .ToString("o");
+
+        return new KlaviyoMember
+        {
+            Data = new KlaviyoMemberData
+            {
+                Attributes = new KlaviyoMemberAttributes
+                {
+                    Email       = reader.IsDBNull(emailOrdinal) ? null : reader.GetString(emailOrdinal),
+                    FirstName   = reader.GetString(reader.GetOrdinal("FirstName")),
+                    LastName    = reader.GetString(reader.GetOrdinal("LastName")),
+                    PhoneNumber = reader.IsDBNull(phoneOrdinal) ? null : reader.GetString(phoneOrdinal),
+                    Birthday    = birthDate,
+                    Location    = city == null && country == null
+                        ? null
+                        : new KlaviyoLocation { City = city, Country = country },
+                    Properties  = new KlaviyoProperties
+                    {
+                        LoyaltyPoints = reader.GetInt32(reader.GetOrdinal("LoyaltyPoints")),
+                        EnrolledAt    = enrolledAt
+                    }
+                }
+            }
+        };
+    }
 }
